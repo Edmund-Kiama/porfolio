7a6e830af7b97266663a090a49ebb1bcc9ab4c03
Edmund <edmundgachanja@gmail.com>
2025-01-26 16:39:46 +0300

    Adds porfolio details

---
+++ src/Home.jsx
@@ -0,0 +1,309 @@
+function Home() {
+  return (
+    <>
+      <header className="header">
+        <h2 className="name">EDMUND KIAMA</h2>
+        <h3 className="location">Nairobi, Kenya</h3>
+        <div className="contacts">
+          <a>Phone</a>
+          <a>Email</a>
+          <a>LinkedIn</a>
+          <a>GitHub</a>
+        </div>
+      </header>
+
+      <div className="dev-stack">
+        <h3>Developer Stack</h3>
+        <ul>
+          <li>
+            <img src="#" alt="logo" />
+            <p>HTML</p>
+          </li> 
+
+          <li>
+            <img src="#" alt="logo" />
+            <p>CSS</p>
+          </li>
+
+          <li>
+            <img src="#" alt="logo" />
+            <p>JavaScript</p>
+          </li>
+
+          <li>
+            <img src="#" alt="logo" />
+            <p>Vue js</p>
+          </li>       
+
+          <li>
+            <img src="#" alt="logo" />
+            <p>React js</p>
+          </li>
+
+          <li>
+            <img src="#" alt="logo" />
+            <p>Python</p>
+          </li>
+
+          <li>
+            <img src="#" alt="logo" />
+            <p>mySQL</p>
+          </li>
+
+          <li>
+            <img src="#" alt="logo" />
+            <p>Git (Version Control)</p>
+          </li>
+
+          <li>
+            <img src="#" alt="logo" />
+            <p>GtiHub</p>
+          </li>
+
+        </ul>
+      </div>
+
+      <div className="education">
+        <div className="Moringa">
+          <div>
+            <img src="#" alt="logo" />
+            <h3>Moringa school</h3>
+          </div>
+          <p>Nov 2024 - March 2025</p>
+          <p><em>Software Engineering | Full-Stack Develeoper</em></p>
+        </div>
+
+        <div className="TUK">
+          <div>
+            <img src="#" alt="logo" />
+            <h3>Technical University of Kenya</h3>
+          </div>
+          <p>July 2019 - Nov 2024</p>
+          <p><em>Second Class Upper Division</em></p>
+          <p><em>Bachelor of Engineering | Mechanical Engineering</em></p>
+        </div>
+      </div>
+
+      <div className="certificates">
+        <div className="harvard">
+          <div>
+            <img src="#" alt="logo"/>
+            <h3>Harvard University</h3>
+          </div>
+          <p>Issued <strong>Oct 2024</strong></p>
+          <p><em>Programming Using Python</em></p>
+        </div>
+
+        <div className="simplilearn">
+          <div>
+            <img src="#" alt="logo"/>
+            <h3>Simplilearn</h3>
+          </div>
+          <p>Issued <strong>Sep 2024</strong></p>
+          <p><em>Introduction of Internet of Things(IoT)</em></p>
+        </div>
+
+        <div className="math-works">
+          <div>
+            <img src="#" alt="logo"/>
+            <h3>MathWorks</h3>
+          </div>
+          <p>Issued <strong>Sep 2024</strong></p>
+          <p><em>MATLAB Certified</em></p>
+        </div>
+
+      </div>
+
+      <div className="experience">
+        <div>
+          <h3>Freelance | Remote</h3>
+          <p>Oct 2024 - Present</p>
+        </div>
+        <div>
+          <h4><em>Front-end Developer</em></h4>
+          <p>Nairobi County, Kenya</p>
+        </div>
+        <ul className="description">
+          <li>Identify and resolve bugs</li>
+          <li>Design and update existing website elements to ensure a visually appealing and user-friendly interface</li>
+          <li>Optimize website content for improved search engine visibility by implementing relevant SEO meta tags</li>
+          <li>Integrate multiple HTML pages by establishing interlinks for seamless navigation</li>
+        </ul>
+      </div>
+
+      <div className="projects">
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <img src="" alt="project-shot"/>
+            <div>
+              <h4>Mini Tik-Tok clone Single Page Application</h4>
+              <p>Sep 2024</p>
+            </div>
+            <ul className="stack-used">
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>HTML</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>CSS</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>JavaScript</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>Vue js</p>
+                </li>
+            </ul>
+            <button>Show More</button>
+          </li>
+
+          <li>
+            <img src="" alt="project-shot"/>
+            <div>
+              <h4>Shopping List Application</h4>
+              <p>Nov 2024</p>
+            </div>
+            <ul className="stack-used">
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>HTML</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>CSS</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>JavaScript</p>
+                </li>
+            </ul>
+            <button>Show More</button>
+          </li>
+
+          <li>
+            <img src="" alt="project-shot"/>
+            <div>
+              <h4>Character Management Application </h4>
+              <p>Nov 2024</p>
+            </div>
+            <ul className="stack-used">
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>HTML</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>CSS</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>JavaScript</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>Json</p>
+                </li>
+            </ul>
+            <button>Show More</button>
+          </li>
+
+          <li>
+            <img src="" alt="project-shot"/>
+            <div>
+              <h4>Tv Show Application</h4>
+              <p>Dec 2024</p>
+            </div>
+            <ul className="stack-used">
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>HTML</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>CSS</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>JavaScript</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>API</p>
+                </li>
+            </ul>
+            <button>Show More</button>
+          </li>
+
+          <li>
+            <img src="" alt="project-shot"/>
+            <div>
+              <h4>E-Commerce Cart for both Developers and Users</h4>
+              <p>Jan 2025</p>
+            </div>
+            <ul className="stack-used">
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>HTML</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>CSS</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>JavaScript</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>React js</p>
+                </li>
+            </ul>
+            <button>Show More</button>
+          </li>
+
+          <li>
+            <img src="" alt="project-shot"/>
+            <div>
+              <h4>4-Wheel Steering Steering System Project</h4>
+              <p>Oct 2023 – March 2024</p>
+            </div>
+            <p>Grade A</p>
+            <ul className="stack-used">
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>SolidWorksL</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>Ultimaker Cura</p>
+                </li>
+                <li>
+                  <img src="#" alt="logo"/>
+                  <p>Blender</p>
+                </li>
+            </ul>
+            <button>Show More</button>
+          </li>
+          
+        </ul>
+      </div>
+
+      <div className="pages-to-add">
+        <h2>Home</h2>
+        <div className="about">
+          <h2>About</h2>
+          <p>Description</p>
+          <p>Extra-curricular</p>
+        </div>
+        <h2>Other Experiences</h2>
+        <h2>Error page</h2>
+      </div>
+    </>
+  )
+}
+
+export default Home
