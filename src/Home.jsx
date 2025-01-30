0da56673dbc7485a20b5cc141fa2dcf7d995e610
Edmund <edmundgachanja@gmail.com>
2025-01-30 10:14:14 +0300

    Finishes experience section

--- src/Home.jsx
+++ src/Home.jsx
@@ -167,6 +167,28 @@ function Home() {
           </ul>
         </div>
 
+        <div className="mech">
+          <div className="details">
+            <div>
+              <h3>National Cement Company Limited</h3>
+              <p>May 2024 - July 2024</p>          
+              <h4><em>Mechanical Engineer | Industrial Attachment</em></h4>
+              <p>Along Mombasa Road, Kenya</p>
+            </div>
+          </div>
+
+          <ul className="description">
+            <li>Ensured plant was in-compliance with Health, Safety and Environment (HSE) standards by inspecting and maintaining emission systems.</li>
+            <li>Assisted in preventative and breakdown maintenance on large machinery such as kiln</li>
+            <li>Assisted in maintaining conveyor systems including belts, pulleys, and idlers, alongside industrial vulcanize engineers</li>
+            <li>Carried out fabrication, welding and fitting tasks in the workshop.</li>
+            <li>Operated on machinery such as lathe,drilling and shaping machine for precise material removal.</li>
+            <li>Assisted in routine inspection and field maintenance with other engineers.</li>
+            <li>Monitored and controlled plant operations in the Central Control Room (CCR) using an integrated mechanical, electrical and software system.</li>
+            <li></li>
+          </ul>
+        </div>
+
       </div>
 
       <div className="projects">
