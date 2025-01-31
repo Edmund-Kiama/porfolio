059b8c0fab9545af3245497b5980f2be554a1368
Edmund <edmundgachanja@gmail.com>
2025-01-31 12:01:57 +0300

    Finishes styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -77,6 +77,16 @@ function Home() {
             <p>Git</p>
           </li>
         </ul>
+        <ul>
+          <li>
+            <img height="20px" src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/SolidWorks_Logo.svg/150px-SolidWorks_Logo.svg.png?20130509090050" alt="logo"/>
+            <p>SolidWorksL</p>
+          </li>
+          <li>
+            <img height="20px" src="https://ultimaker.com/wp-content/themes/ultimaker/media/UltiMaker_Logo.svg" alt="logo"/>
+            <p>Ultimaker Cura</p>
+          </li>
+        </ul>
       </div>
 
       <div className="ed-cert">
@@ -193,176 +203,112 @@ function Home() {
 
       <div className="projects">
         <h2>Projects</h2>
-        <ul>
-          <li>
-            <img src="" alt="project-shot"/>
-            <div>
-              <h4>Mini Tik-Tok clone Single Page Application</h4>
-              <p>Sep 2024</p>
-            </div>
-            <ul className="stack-used">
-              <li>
+        <div className="container">
+            <div className="tiktok">
+              <div className="img-container">
+                <img src="" alt="project-shot"/>
+              </div>
+              
+              <div className="stack-used">
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/640px-HTML5_logo_and_wordmark.svg.png" alt="logo" />
-                <p>HTML</p>
-              </li> 
-
-              <li>
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/640px-CSS.3.svg.png" alt="logo" />
-                <p>CSS</p>
-              </li>
-
-              <li>
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433" alt="logo" />
-                <p>JavaScript</p>
-              </li>
-
-              <li>
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/512px-Vue.js_Logo_2.svg.png?20170919082558" alt="logo" />
-                <p>Vue js</p>
-              </li>
-            </ul>
-            <button>Show More</button>
-          </li>
-
-          <li>
-            <img src="" alt="project-shot"/>
-            <div>
-              <h4>Shopping List Application</h4>
-              <p>Nov 2024</p>
+              </div>
+              <div className="details">
+                <h4>Mini Tik-Tok clone Single Page Application</h4>
+                <p>Sep 2024</p>
+              </div>
+              <button className="more">Show More</button>
             </div>
-            <ul className="stack-used">
-              <li>
+
+            <div className="shopping">
+              <div className="img-container">
+                <img src="" alt="project-shot"/>
+              </div>
+              
+              <div className="stack-used">
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/640px-HTML5_logo_and_wordmark.svg.png" alt="logo" />
-                <p>HTML</p>
-              </li> 
-
-              <li>
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/640px-CSS.3.svg.png" alt="logo" />
-                <p>CSS</p>
-              </li>
-
-              <li>
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433" alt="logo" />
-                <p>JavaScript</p>
-              </li>
-            </ul>
-            <button>Show More</button>
-          </li>
-
-          <li>
-            <img src="" alt="project-shot"/>
-            <div>
-              <h4>Character Management Application </h4>
-              <p>Nov 2024</p>
+              </div>
+              <div className="details">
+                <h4>Shopping List Application</h4>
+                <p>Nov 2024</p>
+              </div>
+              <button className="more">Show More</button>
             </div>
-            <ul className="stack-used">
-              <li>
+            <div className="xter-mgmt">
+              <div className="img-container">
+                <img src="" alt="project-shot"/>
+              </div>
+              <div className="stack-used">
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/640px-HTML5_logo_and_wordmark.svg.png" alt="logo" />
-                <p>HTML</p>
-              </li> 
-
-              <li>
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/640px-CSS.3.svg.png" alt="logo" />
-                <p>CSS</p>
-              </li>
-
-              <li>
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433" alt="logo" />
-                <p>JavaScript</p>
-              </li>
-
-                <li>
-                  <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/JSON_vector_logo.svg/160px-JSON_vector_logo.svg.png?20220501073649" alt="logo"/>
-                  <p>Json</p>
-                </li>
-            </ul>
-            <button>Show More</button>
-          </li>
-
-          <li>
-            <img src="" alt="project-shot"/>
-            <div>
-              <h4>Tv Show Application</h4>
-              <p>Dec 2024</p>
+                <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/JSON_vector_logo.svg/160px-JSON_vector_logo.svg.png?20220501073649" alt="logo"/>
+              </div>
+              <div className="details">
+                <h4>Character Management Application </h4>
+                <p>Nov 2024</p>
+              </div>
+              <button className="more">Show More</button>
             </div>
-            <ul className="stack-used">
-            <li>
+
+            <div className="tv-show">
+              <div className="img-container">
+                <img src="" alt="project-shot"/>
+              </div>
+              <div className="stack-used">
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/640px-HTML5_logo_and_wordmark.svg.png" alt="logo" />
-                <p>HTML</p>
-              </li> 
-
-              <li>
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/640px-CSS.3.svg.png" alt="logo" />
-                <p>CSS</p>
-              </li>
-
-              <li>
                 <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433" alt="logo" />
-                <p>JavaScript</p>
-              </li>
-                <li>
-                  <img  height="25px" src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Api_logo.jpg?20111205072542" alt="logo"/>
-                  <p>API</p>
-                </li>
-            </ul>
-            <button>Show More</button>
-          </li>
-
-          <li>
-            <img src="" alt="project-shot"/>
-            <div>
-              <h4>E-Commerce Cart for both Developers and Users</h4>
-              <p>Jan 2025</p>
+                <img  height="25px" src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Api_logo.jpg?20111205072542" alt="logo"/>
+              </div>
+              <div className="details">
+                <h4>Tv Show Application</h4>
+                <p>Dec 2024</p>
+              </div>
+              <button className="more">Show More</button>
             </div>
-            <ul className="stack-used">
-              <li>
-                <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/640px-HTML5_logo_and_wordmark.svg.png" alt="logo" />
-                <p>HTML</p>
-              </li> 
 
-              <li>
-                <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/640px-CSS.3.svg.png" alt="logo" />
-                <p>CSS</p>
-              </li>
-
-              <li>
-                <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433" alt="logo" />
-                <p>JavaScript</p>
-              </li>       
-
-              <li>
-                <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt="logo" />
-                <p>React js</p>
-              </li>
-            </ul>
-            <button>Show More</button>
-          </li>
-
-          <li>
-            <img src="" alt="project-shot"/>
-            <div>
-              <h4>4-Wheel Steering Steering System Project</h4>
-              <p>Oct 2023 – March 2024</p>
+            <div className="cart">
+              <div className="img-container">
+                <img src="" alt="project-shot"/>
+              </div>
+              <div className="stack-used">
+                  <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/640px-HTML5_logo_and_wordmark.svg.png" alt="logo" />
+                  <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/640px-CSS.3.svg.png" alt="logo" />
+                  <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433" alt="logo" />
+                  <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" alt="logo" />
+              </div>
+              <div className="details">
+                <h4>E-Commerce Cart for both Developers and Users</h4>
+                <p>Jan 2025</p>
+              </div>
+              <button className="more">Show More</button>
             </div>
-            <p>Grade A</p>
-            <ul className="stack-used">
-                <li>
-                  <img height="20px" src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/SolidWorks_Logo.svg/150px-SolidWorks_Logo.svg.png?20130509090050" alt="logo"/>
-                  <p>SolidWorksL</p>
-                </li>
-                <li>
-                  <img height="20px" src="https://ultimaker.com/wp-content/themes/ultimaker/media/UltiMaker_Logo.svg" alt="logo"/>
-                  <p>Ultimaker Cura</p>
-                </li>
-                <li>
-                  <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Blender_logo_no_text.svg/640px-Blender_logo_no_text.svg.png" alt="logo"/>
-                  <p>Blender</p>
-                </li>
-            </ul>
-            <button>Show More</button>
-          </li>
-          
-        </ul>
+
+            <div className="ws4">
+              <div className="img-container">
+                <img 
+                  src="https://media.licdn.com/dms/image/v2/D562DAQFRxYl3V37CSQ/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1736341258826?e=1738915200&v=beta&t=C6HBULQ85ca7JVc9f-hXCcFC985QdhKpA2Bod7JV10w" 
+                  alt="4ws-render"  
+                  className="render"
+                />
+              </div>
+              <div className="stack-used">
+                    <img height="25px" src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/SolidWorks_Logo.svg/150px-SolidWorks_Logo.svg.png?20130509090050" alt="logo"/>
+                    <img height="20px" src="https://ultimaker.com/wp-content/themes/ultimaker/media/UltiMaker_Logo.svg" alt="logo"/>
+                    <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Blender_logo_no_text.svg/640px-Blender_logo_no_text.svg.png" alt="logo"/>
+              </div>
+              <div className="details">
+                <h4>4-Wheel Steering Steering System</h4>
+                <p>Grade A | Oct 2023 – March 2024</p>
+              </div>          
+              <button className="more">Show More</button>
+            </div>
+        </div>
       </div>
 
       <div className="pages-to-add">
