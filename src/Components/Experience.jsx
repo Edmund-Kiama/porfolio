da6bf3ed88b4f6fdc6127eef4a150900d151c7c9
Edmund <edmundgachanja@gmail.com>
2025-02-11 13:06:53 +0300

    Refines the experience section

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -7,16 +7,16 @@ export default function Experiences () {
             <div>
               <h3>Freelance | Remote</h3>
               <p>Oct 2024 - Present</p>          
-              <h4><em>Front-end Developer</em></h4>
+              <h4><em>Software Engineer</em></h4>
               <p>Nairobi County, Kenya</p>
             </div>
           </div>
 
           <ul className="description">
-            <li>Identify and resolve bugs</li>
-            <li>Design and update existing website elements to ensure a visually appealing and <br/>user-friendly interface</li>
-            <li>Optimize website content for improved search engine visibility by implementing <br/>relevant SEO meta tags</li>
-            <li>Integrate multiple HTML pages by establishing interlinks for seamless navigation</li>
+            <li>Identify and resolve bugs to improve application functionality and performance.</li>
+            <li>Develop scalable and resilient applications to ensure reliability and efficiency.</li>
+            <li>Integrate CRUD operations to enhance application functionality and user experience.</li>
+            <li>Collaborate with a team of software engineers to design and implement robust, high-performing applications.</li>
           </ul>
         </div>
 
@@ -31,14 +31,10 @@ export default function Experiences () {
           </div>
 
           <ul className="description">
-            <li>Ensured plant was in-compliance with Health, Safety and Environment (HSE) standards by inspecting and maintaining emission systems.</li>
-            <li>Assisted in preventative and breakdown maintenance on large machinery such as kiln</li>
-            <li>Assisted in maintaining conveyor systems including belts, pulleys, and idlers, alongside industrial vulcanize engineers</li>
-            <li>Carried out fabrication, welding and fitting tasks in the workshop.</li>
-            <li>Operated on machinery such as lathe,drilling and shaping machine for precise material removal.</li>
-            <li>Assisted in routine inspection and field maintenance with other engineers.</li>
-            <li>Monitored and controlled plant operations in the Central Control Room (CCR) using an integrated mechanical, electrical and software system.</li>
-            <li></li>
+            <li>Ensured operations were in compliance to operational and safety standards by monitoring and maintaining automated control systems.</li>
+            <li>Supported engineers in preventive and corrective maintenance of industrial systems, contributing to system reliability and uptime.</li>
+            <li>Inspected and maintained automated conveyor and material handling systems with senior engineers.</li>
+            <li>Monitoring and controlling plant operations from the Central Control Room (CCR) using an integrated software, electrical and mechanical system</li>
           </ul>
         </div>
 
